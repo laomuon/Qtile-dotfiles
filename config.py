f56847ef65d1814e1db86818b1fd550d6ca7df02
laomuon <laomuon@github.com>
2023-10-30 14:28:39 +0100

    Disable follow_mouse_focus

--- config.py
+++ config.py
@@ -286,7 +286,7 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
+follow_mouse_focus = False
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(
